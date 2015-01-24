6be4c2dc604e21568f2b6777650bd61486d3a748
Joshua Landman <jsl9284@rit.edu>
2015-01-23 20:03:33 -0500

    Setup
    We have the folders, basic player movement, camera, and textures

---
+++ Game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	//insert object to follow
+	public Transform followObject;
+
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//set a position slightly behing the object
+		Vector3 pos = new Vector3
+			(followObject.transform.position.x,
+			followObject.transform.position.y + 10f,
+			 followObject.transform.position.z);
+
+		//set self to position
+		transform.position = pos;
+	}
+}
---
+++ Game/Assets/Scripts/Movement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	float speed = .1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetKeyDown(KeyCode.W))
+		{
+			transform.position += speed * transform.forward;
+		}
+		if(Input.GetKeyDown(KeyCode.D))
+		{
+			transform.position += speed * transform.right;
+		}
+		if(Input.GetKeyDown(KeyCode.S))
+		{
+			transform.position += speed * -transform.forward;
+		}
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			transform.position += speed * -transform.right;
+		}
+	}
+}
