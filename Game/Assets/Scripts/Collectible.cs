4d49cef3a065b3c9da839a00bd9cb6970ea7f0eb
MrNex <npg3253@rit.edu>
2015-01-23 21:29:25 -0500

    Add Collectible script.

---
+++ Game/Assets/Scripts/Collectible.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectible : MonoBehaviour {
+
+	ItemType type;
+	int amount;
+
+	// Use this for initialization
+	void Start (ItemType t, int amt) {
+		type = t;
+		amount = amt;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
