773b378326f20b497ea1cf53aed0ef22b0fd2473
MrNex <npg3253@rit.edu>
2015-01-23 21:30:12 -0500

    Added constants file

---
+++ Game/Assets/Scripts/MyConstants.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum ItemType
+{
+	SHOVEL, AXE, WOOD, FEATHERS, MATCHES, KNIFE, FLINT, ROPE
+}
+
+
+public class MyConstants {
+	public const int NUM_ITEM_TYPES = 8;
+
+
+}
