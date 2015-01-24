f4689385c4f66b56d14bf287632198b770300884
Joshua Landman <jsl9284@rit.edu>
2015-01-24 18:02:16 -0500

    Altar Scripts
    Andrew wrote altar scripts and torch script

---
+++ Game/Assets/Scripts/Altar.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Altar : Interactable {
+
+	private Torch[] torches = new Torch[4];
+
+	// Use this for initialization
+	void Start () {
+		//Create 4 torches around the altar
+		float _x = transform.position.x;
+		float _y = transform.position.y;
+		float _z = transform.position.z;
+		torches[0] = new Torch(_x, _y, _z);
+		torches[1] = new Torch(_x+10, _y, _z);
+		torches[2] = new Torch(_x+20, _y, _z);
+		torches[3] = new Torch(_x+30, _y, _z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Game/Assets/Scripts/Torch.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Torch : Interactable {
+
+	bool isLit = false;
+	public GameObject torch;
+
+	public Torch(float x, float y, float z)
+	{
+		torch = new GameObject("Torch");
+		torch.transform.position = new Vector3(x, y, z);
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isLit)
+		{
+			//fire animation
+		}
+	}
+
+	public void Interact(GameObject obj)
+	{
+		Player p = obj.GetComponent<Player>();
+		if(p.inventory.ContainsItem(ItemType.MATCHES))
+		{
+			isLit = !isLit;
+		}
+		else
+		{
+			isLit = false;
+		}
+	}
+}
