6857d218946c5de95dc391bc67e43a9a47accaf2
CaptKH <khewitt171@gmail.com>
2015-01-25 13:15:26 -0500

    Game Over Screen
    Added

---
+++ Game/Assets/Scripts/GameOverScreen.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+		if (GUI.Button (new Rect (Screen.width / 3 - Screen.width / 10, Screen.height * 0.8f, Screen.width / 5, Screen.height / 10), "Continue?")) {
+			Application.LoadLevel ("Game");
+		}
+
+		if (GUI.Button (new Rect ((Screen.width / 3 * 2) - Screen.width / 10, Screen.height * 0.8f, Screen.width / 5, Screen.height / 10), "Quit.")) {
+			Application.Quit();
+		}
+	}
+}
