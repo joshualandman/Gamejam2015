d6561c9e506a314093827241839608fe9fe211ac
Joshua Landman <jsl9284@rit.edu>
2015-01-25 11:32:20 -0500

    SHIP ENDING
    Ship ends

---
+++ Game/Assets/Scripts/Ship.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : Interactable {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public override void Interact(GameObject obj)
+	{
+		Player p = obj.GetComponent<Player> ();
+		MyConstants.Win(6);
+	}
+}
